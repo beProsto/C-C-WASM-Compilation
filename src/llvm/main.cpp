f3c79372ccf1e970283b1c81f55d388d05120bf5
beProsto <lavionperavion2@gmail.com>
2022-01-04 01:27:59 +0100

    added a fun little way to import a function

--- src/llvm/main.cpp
+++ src/llvm/main.cpp
@@ -1,14 +1,19 @@
 // This imports the functions we defined in JS, in the "env" block
-extern "C" void __wasm_import_console_log_add_to_buffer(char);
-extern "C" void __wasm_import_console_log_flush_buffer();
+// extern "C" void __wasm_import_console_log_str(const char*);
+extern "C" void __wasm_import_console_log_str(char*);
+extern "C" void __wasm_import_winreqanim_call();
 
-// A custom implementation of console_log
-void console_log(const char* _str) {
-	for(int i = 0; _str[i] != 0; i++) {
-		__wasm_import_console_log_add_to_buffer(_str[i]);
-	}
-	__wasm_import_console_log_flush_buffer();
+extern "C" void (*__wasm_winreqanim_callback)();
+
+extern "C" void __wasm_export_winreqanim_exec() {
+	__wasm_winreqanim_callback();
 }
+
+extern "C" void __wasm_set_winreqanim_callback(void (*_cb)()) {
+	__wasm_winreqanim_callback = _cb;
+	__wasm_import_winreqanim_call();
+}
+
 
 const char* HELLO_WORLD_STR = "LLVM Compiled and run succsessfully!";
 
@@ -16,6 +21,13 @@ const char* HELLO_WORLD_STR = "LLVM Compiled and run succsessfully!";
 // (because namespaces etc)
 // so we have to use this special keyword, `extern "C"`
 // to preserver our function's name in the resulting wasm binary
+
+void animation_frame() {
+	__wasm_import_console_log_str((char*)"Animation Frame Update!");
+	__wasm_set_winreqanim_callback(animation_frame);
+}
+
 extern "C" void hello() {
-	console_log(HELLO_WORLD_STR);
+	__wasm_import_console_log_str((char*)HELLO_WORLD_STR);
+	__wasm_set_winreqanim_callback(animation_frame);
 }
