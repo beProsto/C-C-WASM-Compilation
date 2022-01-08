d7f3cadab08ff244bfa38007d3580d11dd9a2ecd
beProsto <lavionperavion2@gmail.com>
2022-01-08 16:07:22 +0100

    created utils

--- src/llvm/main.cpp
+++ src/llvm/main.cpp
@@ -1,41 +1,6 @@
-// This imports the functions we defined in JS, in the "env" block
-// extern "C" void __wasm_import_console_log_str(const char*);
-extern "C" void __wasm_import_console_log_str(const char*);
-extern "C" void __wasm_import_console_log_num(int);
-// calls window.requestAnimationFrame, passing the __wasm_export_winreqanim_exec function as the one to play on the frame
-extern "C" void __wasm_import_winreqanim_call();
-
-void (*__wasm_winreqanim_callback)();
-// simply calls our dynamic callback - done so that the callback can be changed and is not set to be a static function
-extern "C" void __wasm_export_winreqanim_exec() {
-	__wasm_winreqanim_callback();
-}
-
-// sets the callback to one we'd need and then calls the window.requestAnimationFrame function
-extern "C" void __wasm_set_winreqanim_callback(void (*_cb)()) {
-	__wasm_winreqanim_callback = _cb;
-	__wasm_import_winreqanim_call();
-}
-
-
-// import memory stuff
-extern "C" void* __wasm_import_malloc(unsigned int);
-extern "C" void __wasm_import_free(void*);
-
-template<typename T> T* make(unsigned int _ammount = 1) {
-	return (T*)__wasm_import_malloc(_ammount * sizeof(T));
-}
-template<typename T> void free(T* _ptr) {
-	return __wasm_import_free(_ptr);
-}
-
+#include "utils/utils.hpp"
 
 const char* HELLO_WORLD_STR = "LLVM Compiled and run succsessfully!";
-
-// as opposed to C, in C++ the compiler will do weird things to the function names in the binary
-// (because namespaces etc)
-// so we have to use this special keyword, `extern "C"`
-// to preserver our function's name in the resulting wasm binary
 
 // these functions will define what happens per frame
 void (*pointer_to_animation_frame_first)();
@@ -50,28 +15,32 @@ void animation_frame_first() {
 	__wasm_set_winreqanim_callback(animation_frame_second);
 }
 
+// as opposed to C, in C++ the compiler will do weird things to the function names in the binary
+// (because namespaces etc)
+// so we have to use this special keyword, `extern "C"`
+// to preserver our function's name in the resulting wasm binary
 extern "C" void hello() {
 	__wasm_import_console_log_str(HELLO_WORLD_STR);
 	__wasm_import_console_log_str("If you wish to stop the Animation Frame cycle (the text that keeps on being printed), press Escape.");
 
 	__wasm_import_console_log_str("Testing malloc:");
 
-	// allocation
-	int* a = make<int>(5);
-	int* b = a;
+	// // allocation
+	// int* a = make<int>(5);
+	// int* b = a;
 
-	*b = 5;
-	b++;
-	*b = 10;
-	b++;
-	*b = 63;
+	// *b = 5;
+	// b++;
+	// *b = 10;
+	// b++;
+	// *b = 63;
 	
 
-	for(int i = 0; i < 5; i++) {
-		__wasm_import_console_log_num(a[i]);
-	}
-	// deallocation
-	free(a);
+	// for(int i = 0; i < 5; i++) {
+	// 	__wasm_import_console_log_num(a[i]);
+	// }
+	// // deallocation
+	// free(a);
 	
 	__wasm_import_console_log_str("Testing malloc ended!");
 	
---
+++ src/llvm/utils/utils.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "imports.hpp"
+#include "winreqanim.hpp"
