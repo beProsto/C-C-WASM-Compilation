135b0148c270178a61524ce5665efad62c53f9c3
beProsto <lavionperavion2@gmail.com>
2022-01-14 21:16:12 +0100

    renamed build to public for logic reasons

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ In this project I used clang to compile C/C++ files to WASM.
 
 To compile and run the project on your own, you're going to have to run the `run.cmd` file.
 
-[Check out the build here (a page that loads the compiled code)!](https://beprosto.github.io/C-CPP-WASM-Compilation/build/)
+[Check out the public here (a page that loads the compiled code)!](https://beprosto.github.io/C-CPP-WASM-Compilation/public/)
 
 A giant shoutout to [@maxkl](https://github.com/maxkl) for creating a genious [wasm-malloc](https://github.com/maxkl/wasm-malloc) repo, that I got the malloc implementation from.
 
--- run.cmd
+++ run.cmd
@@ -2,24 +2,24 @@
 
 echo Compiling the simple llvm code:
 :: compiles the simpler, c file that simply exports 1 function
-powershell clang --target=wasm32 -nostdlib '-Wl,--no-entry' '-Wl,--export-all' -o build/llvm/simple.wasm src/llvm/simple.c
+powershell clang --target=wasm32 -nostdlib '-Wl,--no-entry' '-Wl,--export-all' -o public/llvm/simple.wasm src/llvm/simple.c
 echo Finished!
 echo ---
 
 echo Compiling the advanced llvm code:
 :: compiles the weirder c++ file that not only exports a function, but also imports a couple
 :: ('-Wl,--allow-undefined' let's us define functions that our js code imports without needing to write any implementation for them in the c++ code)
-powershell clang++ --target=wasm32 -std=c++17 -nostdlib '-Wl,--no-entry' '-Wl,--export-all' '-Wl,--allow-undefined' -o build/llvm/main.wasm src/llvm/main.cpp src/llvm/utils/src/malloc.c src/llvm/utils/src/winreqanim.cpp
+powershell clang++ --target=wasm32 -std=c++17 -nostdlib '-Wl,--no-entry' '-Wl,--export-all' '-Wl,--allow-undefined' -o public/llvm/main.wasm src/llvm/main.cpp src/llvm/utils/src/malloc.c src/llvm/utils/src/winreqanim.cpp
 echo Finished!
 echo ---
 
 echo Compiling the WAT code:
 :: uses wat2wasm tool from wabt toolkit
-powershell wat2wasm src/wat/testin_it.wat -o build/wat/testin_it.wasm
+powershell wat2wasm src/wat/testin_it.wat -o public/wat/testin_it.wasm
 echo Finished!
 echo ---
 
-cd build
+cd public
 echo Running the Server on http://localhost:8080/
 :: runs a python server to show the results
 python -m http.server 8080
